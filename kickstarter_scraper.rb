6a2644959e32f6de2262c0eb03bb4e9e7cf9a3c9
Justin Tooley <jstooley823@gmail.com>
2017-09-16 12:27:00 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,5 +12,8 @@ def create_project_hash
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project]= {}
+  end
 end
 create_project_hash
